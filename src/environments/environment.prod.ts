76d1b30f93c76c157f778b8d4144ea217c6ceaa9
Alex Pulido <axpalx@gmail.com>
2020-11-25 14:56:30 -0300

    Previsao v 1.0

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiKey: '8be3bb2e8720b885bc2e8fe1f71e6b58',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiKey: '8be3bb2e8720b885bc2e8fe1f71e6b58',
+};
